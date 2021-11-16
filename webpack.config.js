6decc624fd92f07cc6208669cc96e975483365e5
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-16 13:18:54 -0800

    first commit
    co-authored-by: Albert Lee <leesga8@gmail.com>
    co-authored-by: Christy Welch <christinamawel@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const DotenvWebpackPlugin = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,6 +14,7 @@ module.exports = {
     contentBase: './dist'
   },
   plugins: [
+    new DotenvWebpackPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
